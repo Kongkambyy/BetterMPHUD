91f76fa06f33700dcc1827104193bf59062a01f8
kongkambyy <joachimkambyn@gmail.com>
2025-12-17 20:35:14 +0100

    Initial commit

---
+++ BetterMPHUD/HudMenuVM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using TaleWorlds.Library;
+
+namespace BetterMPHUD.ViewModels
+{
+    public class HudMenuVM : ViewModel
+    {
+        private bool _isConfigMenuOpen;
+        public Action OnCloseConfigMenu;
+
+        public HudMenuVM()
+        {
+            // Constructor code (load settings) would go here later
+        }
+
+        public void ExecuteClose()
+        {
+            OnCloseConfigMenu?.Invoke();
+        }
+
+        [DataSourceProperty]
+        public bool IsConfigMenuOpen
+        {
+            get => _isConfigMenuOpen;
+            set
+            {
+                if (value != _isConfigMenuOpen)
+                {
+                    _isConfigMenuOpen = value;
+                    OnPropertyChangedWithValue(value, "IsConfigMenuOpen");
+                }
+            }
+        }
+    }
+}
---
+++ BetterMPHUD/SubModule.cs
@@ -0,0 +1,15 @@
+﻿using TaleWorlds.MountAndBlade;
+using BetterMPHUD.Views;
+
+namespace BetterMPHUD
+{
+    public class SubModule : MBSubModuleBase
+    {
+        public override void OnMissionBehaviorInitialize(Mission mission)
+        {
+            base.OnMissionBehaviorInitialize(mission);
+            // Add our View to the mission
+            mission.AddMissionBehavior(new HudMissionView());
+        }
+    }
+}
