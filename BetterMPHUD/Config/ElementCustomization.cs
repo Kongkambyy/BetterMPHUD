4cb0d41bdc637e928d06a8764c65e936e9d45004
kongkambyy <joachimkambyn@gmail.com>
2025-12-18 14:05:53 +0100

    a

---
+++ BetterMPHUD/Config/ElementCustomization.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BetterMPHUD
+{
+    [Serializable]
+    public class ElementCustomization
+    {
+        public float OffsetX { get; set; } = 0f;
+        public float OffsetY { get; set; } = 0f;
+        public float Scale { get; set; } = 1f;
+
+        public ElementCustomization() { }
+
+        public ElementCustomization(float offsetX, float offsetY, float scale)
+        {
+            OffsetX = offsetX;
+            OffsetY = offsetY;
+            Scale = scale;
+        }
+
+        public void Reset()
+        {
+            OffsetX = 0f;
+            OffsetY = 0f;
+            Scale = 1f;
+        }
+    }
+
+    public enum HudElement
+    {
+        TimeAndScores,
+        TeamAvatars,
+        Morale
+    }
+}
