232bf82256cffa2c002fb069dfb8a02f3d895583
Milan Döpper <milan.doepper@gmail.com>
2025-12-22 13:47:06 +0100

    Patched native compatible

--- BetterMPHUD/SubModule.cs
+++ BetterMPHUD/SubModule.cs
@@ -1,7 +1,12 @@
 ﻿using BetterMPHUD.Behaviors;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
 using TaleWorlds.Core;
 using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.Diamond;
 
 namespace BetterMPHUD
 {
@@ -18,6 +23,21 @@ namespace BetterMPHUD
             base.OnMissionBehaviorInitialize(mission);
             mission.AddMissionBehavior(new HudBehavior());
             InformationManager.DisplayMessage(new InformationMessage("BetterMPHUD Behavior Added", Colors.Cyan));
+        }
+
+        public override async void OnInitialState()
+        {
+            base.OnInitialState();
+            var field = NetworkMain.GameClient.GetType().GetField("_loadedUnofficialModules", BindingFlags.Instance | BindingFlags.NonPublic);
+
+            List<ModuleInfoModel> moduleList;
+            while ((moduleList = field.GetValue(NetworkMain.GameClient) as List<ModuleInfoModel>) == null)
+                await Task.Delay(1);
+
+            field.SetValue(NetworkMain.GameClient, moduleList.Where(m => m.Id != "BetterMPHUD").ToList());
+
+            InformationManager.DisplayMessage(new InformationMessage("Fuck anime"));
+
         }
     }
 }
